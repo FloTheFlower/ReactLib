e6b55768f7df832a434a0c45771c07fdabdc9576
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-08 13:54:32 -0800

    added first coponent called EventDashboard for columns

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
+import EventDashboard from './event/EventDashboard/EventDashboard';
+
 
 class App extends Component {
   render() {
   return (
     <div>
     <h1>Gurkenman</h1>
-    <button className="ui icon button"> 
-    <i className="smile icon"></i>
-    CSS button 
-    </button>
-    </div>
+  <EventDashboard />
+        </div>
   )
   }
 }
