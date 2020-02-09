e0051e277ddc9bf54734295474e5507e371657c4
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-08 19:02:14 -0800

    pass data from user via form into component in push array of events

--- src/app/event/EventDashboard/EventDashboard.jsx
+++ src/app/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import cuid from 'cuid'
 
 
 const eventsFromDashboard = [
@@ -62,13 +63,30 @@ const eventsFromDashboard = [
 
     state = {
         events: eventsFromDashboard,
-        isOpen: false
+        isOpen: false,
+        selectedEvent: null
     };
 
     handleIsOpenToggle = () => {
         this.setState(({isOpen}) => ( {
             isOpen: !isOpen
         }))
+    }
+
+    handleCreateEvent = (newEvent) => {
+        newEvent.id = cuid();
+        newEvent.hostPhotoURL = '/assets/user.png';
+        this.setState(({events}) => ({
+            events: [...events, newEvent],
+            isOpen: false
+        }))
+    }
+
+    handleSelectEvent = (event ) => {
+        this.setState({
+            selectedEvent: event,
+            isOpen: true
+        })
     }
 
     render() {
@@ -80,7 +98,8 @@ const eventsFromDashboard = [
                 </Grid.Column>
                 <Grid.Column width= {6}>
                     <Button onClick={this.handleIsOpenToggle} positive content= 'Create Event' />
-                    {isOpen && <EventForm cancelFormOpen = {this.handleIsOpenToggle}  />}
+                    {isOpen && <EventForm createEvent={this.handleCreateEvent}
+                    cancelFormOpen = {this.handleIsOpenToggle}  />}
                     
                 </Grid.Column>
 
