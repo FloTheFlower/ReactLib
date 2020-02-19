acbf71bf3e78e6760fe2c89e0e3b9bea45f97de4
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-18 16:22:42 -0800

    added exact routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,12 @@ import EventDashboard from "./event/EventDashboard/EventDashboard";
 import NavBar from "./features/nav/Navbar/NavBar";
 import { Container } from "semantic-ui-react";
 import { Route } from "react-router-dom";
+import { HomePage } from "./event/home/HomePage";
+import { EventDetailedPage } from "./event/EventDetailed/EventDetailedPage";
+import { PeopleDashboard } from "./event/User/PeopleDashBoard/PeopleDashboard";
+import { SettingsDashboard } from "./event/Settings/SettingsDashboard";
+import EventForm from "./event/EventForm/EventForm";
+import { UserDetailedPage } from "./event/UserDetailed/UserDetailedPage";
 
 
 class App extends Component {
@@ -13,7 +19,13 @@ class App extends Component {
         <Fragment>
           <NavBar />
           <Container className="main">
+            <Route exact path ='/' component={HomePage } />
             <Route path ='/events' component={EventDashboard} />
+            <Route path ='/events/:id' component={EventDetailedPage} />
+            <Route path ='/people' component={PeopleDashboard} />
+            <Route path ='/profile/:id' component={UserDetailedPage} />
+            <Route path ='/settings' component={SettingsDashboard} />
+            <Route path ='/createEvent' component={EventForm} />
           </Container>
         </Fragment>
       </div>
