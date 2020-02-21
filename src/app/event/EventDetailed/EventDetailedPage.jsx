f1b7a2e7865cd296b2520317f6e27028661a5a97
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-21 10:48:37 -0800

    Revert "not working"
    This reverts commit 59379cd0aca6617ee86b4aa736d9b8d4e2ad2fa1.

--- src/app/event/EventDetailed/EventDetailedPage.jsx
+++ src/app/event/EventDetailed/EventDetailedPage.jsx
@@ -4,28 +4,41 @@ import { EventDetailedHeader } from './EventDetailedHeader'
 import { EventDetailedInfo } from './EventDetailedInfo'
 import { EventDetailedChat } from './EventDetailedChat'
 import { EventDetailedSidebar } from './EventDetailedSidebar'
-import { connect } from 'react-redux';
 
 
- const mapState = (state, ownProps) => {
-     const eventId = ownProps.match.params.id;
-     let event = {};
 
-     if (eventId && state.events.length > 0){
-        event = state.events.filter(event => event.id === eventId)[0]
-     }
-     return {
-         event
-     }
-    
- }
+const event =   
+    {
+      id: "1",
+      title: "Trip to Tower of London",
+      date: "2018-03-27",
+      category: "culture",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+      city: "London, UK",
+      venue: "Tower of London, St Katharine's & Wapping, London",
+      hostedBy: "Bob",
+      hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+      attendees: [
+        {
+          id: "a",
+          name: "Bob",
+          photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        },
+        {
+          id: "b",
+          name: "Tom",
+          photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        }
+    ]
+}
 
 
-export const EventDetailedPage = ({event}) => {
+export const EventDetailedPage = () => {
     return (
        <Grid>
             <Grid.Column width={10}>
-                <EventDetailedHeader event={event} />
+                <EventDetailedHeader event ={event} />
                 <EventDetailedInfo event = {event} />
                 <EventDetailedChat />
 
@@ -38,5 +51,3 @@ export const EventDetailedPage = ({event}) => {
        </Grid>
     )
 }
-
-export default connect(mapState) (EventDetailedPage);
