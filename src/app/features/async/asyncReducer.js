31b2f19f6074220b938b09878c75b42b343fd884
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-27 15:11:27 -0800

    making sure only the button that loads shows lading flag in testarea

--- src/app/features/async/asyncReducer.js
+++ src/app/features/async/asyncReducer.js
@@ -1,24 +1,39 @@
+import { createReducer } from "../../common/util/reducerUtils"
+import { ASYNC_ACTION_START, ASYNC_ACTION_FINSIH, ASYNC_ACTION_ERROR } from "./asyncConstants"
+import { asyncActionFinish } from "./asyncActions"
+
 const initalState = {
-    loading: false
+    loading: false, 
+    elementName: null
 }
 
-const asyncActionStarted = (state) => {
+const asyncActionStarted = (state, payload) => {
     return {
         ...state,
-        loading: true
+        loading: true,
+        elementName: payload
+
     }
 }
 
 const asyncActionFinished = (state) => {
     return {
         ...state,
-        loading: false
+        loading: false,
+        elementName: null
     }
 }
 
 const asyncActionError = (state) => {
     return {
         ...state,
-        loading: false
+        loading: false,
+        elementName: null
     }
 }
+
+export default createReducer(initalState, {
+    [ASYNC_ACTION_START]: asyncActionStarted,
+    [ASYNC_ACTION_FINSIH]: asyncActionFinish, 
+    [ASYNC_ACTION_ERROR]: asyncActionError
+})
