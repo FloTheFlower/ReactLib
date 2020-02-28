0aa315240e6093f4ded4d50e066041cdaf35b9bd
FloTheFlower <florentina.hofbauer@gmail.com>
2020-02-28 15:22:08 -0800

    firebase login

--- src/app/features/auth/authActions.js
+++ src/app/features/auth/authActions.js
@@ -1,17 +1,32 @@
-import { LOGIN_USER, SIGN_OUT_USER } from "./authConstants";
+import { SIGN_OUT_USER } from "./authConstants";
 import { closeModal } from "../modals/modalActions";
+import firebase from '../../../app/config/firebase';
 
-export const login = creds => {
-  return dispatch => {
-    dispatch({
-      type: LOGIN_USER,
-      payload: {
-        creds
-      }
-    });
-    dispatch(closeModal())
+
+export const login = (creds) => {
+  return async (dispatch) => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+      dispatch(closeModal());
+ 
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
+
+
+// export const login = creds => {
+//   return asnync (dispatch, getState, { getFirebase }) => {
+//     const firebase = getFirebase();
+//     try {
+//       await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password)
+//       dispatch(closeModal())
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   };
+// };
 
 export const logout = () => {
   return {
