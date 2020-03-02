a5e09e7b40413e3277ab37c83846b46e02c55f5c
FloTheFlower <florentina.hofbauer@gmail.com>
2020-03-02 08:57:09 -0800

    setting up basic page with enableReainitalize so taht it fills in automatically when a user is logged in

--- src/app/event/Settings/SettingsDashboard.jsx
+++ src/app/event/Settings/SettingsDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
 import { SettingsNav } from './SettingsNav'
 import { Route, Redirect, Switch } from 'react-router-dom'
-import { BasicPage } from './BasicPage'
+import  BasicPage  from './BasicPage'
 import { AboutPage } from './AboutPage'
 import { PhotosPage } from './PhotosPage'
 import AccountPage  from './AccountPage'
@@ -14,16 +14,17 @@ const actions = {
 }
 
 const mapState = (state) => ({
-    providerId: state.firebase.auth.providerData[0].providerId
+    providerId: state.firebase.auth.providerData[0].providerId,
+    user: state.firebase.profile
 }) 
 
-const SettingsDashboard = ({updatePassword, providerId}) => {
+const SettingsDashboard = ({updatePassword, providerId, user}) => {
     return (
         <Grid>
             <GridColumn width={12}>
                 <Switch> 
                 <Redirect exact from ='/settings' to='/settings/basic' />
-                < Route path = '/settings/basic' component={BasicPage} /> 
+                < Route path = '/settings/basic' render = {() => <BasicPage initialValues={user}/>} />
                 < Route path = '/settings/about' component={AboutPage} /> 
                 < Route path = '/settings/photos' component={PhotosPage} /> 
                 < Route path = '/settings/account'  render={()=> <AccountPage  updatePassword={updatePassword} providerId ={providerId} />} /> 
