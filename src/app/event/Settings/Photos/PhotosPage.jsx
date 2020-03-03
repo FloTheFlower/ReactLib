e294f6c81dc6c5e479b85f55491a736e697a4143
FloTheFlower <florentina.hofbauer@gmail.com>
2020-03-03 13:24:36 -0800

    adding a close button and a save button on the uploaded pics

--- src/app/event/Settings/Photos/PhotosPage.jsx
+++ src/app/event/Settings/Photos/PhotosPage.jsx
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
+import {connect} from 'react-redux';
+import toastr from 'react-redux-toastr';
 import {
   Image,
   Segment,
@@ -9,15 +11,45 @@ import {
   Card
 } from "semantic-ui-react";
 import DropzoneInput from "./DropzoneInput";
+import {uploadProfileImage} from '../../User/userActions'
 
-const PhotosPage = () => {
+const actions = {
+  uploadProfileImage
+}
+
+
+
+const PhotosPage = ({uploadProfileImage}) => {
  const [files, setFiles] = useState([]);
+const [image, setImage] = useState(null)
 
 useEffect(() => {
   return () => {
     files.forEach(file => URL.revokeObjectURL(file.preview))
   }
 }, [files])
+
+ 
+
+const handleUploadImage = async() => {
+  try{
+
+    await uploadProfileImage(image, files[0].name)
+    toastr.success('Success', 'Photo has been uploaded')
+
+  } catch(error) {
+    console.log(error);
+    toastr.error('Ooops', 'Something went wrong')
+  }
+}
+
+
+const handleCacnelCrop = () => {
+    setFiles([]);
+    setImage(null)
+
+}
+
 
     return (
       <Segment>
@@ -35,12 +67,26 @@ useEffect(() => {
           <Grid.Column width={1} />
           <Grid.Column width={4}>
             <Header sub color="teal" content="Step 3 - Preview & Upload" />
-            {files.length > 0 && 
+            {files.length > 0 && (
+              <Fragment>
             
             <Image src={files[0].preview} style={{minHeight: '200px', minWidth: '200px'}} />
-            }
+
+            <Button.Group>
+
+              <Button onClick={handleUploadImage} style={{width: '100px'}} positive icon='check' />
+              <Button onClick={handleCacnelCrop} style={{width: '100px'}} icon='close' />
+             
+
+            </Button.Group>
+            </Fragment>
+           )}
+
+
           </Grid.Column>
         </Grid>
+
+
 
         <Divider />
         <Header sub color="teal" content="All Photos" />
@@ -60,10 +106,11 @@ useEffect(() => {
               <Button basic icon="trash" color="red" />
             </div>
           </Card>
-        </Card.Group>
+        </Card.Group> 
+
       </Segment>
     );
   }
 
 
-export default PhotosPage;
+export default connect(null, actions)(PhotosPage);
--- src/app/event/User/userActions.js
+++ src/app/event/User/userActions.js
@@ -1,4 +1,5 @@
 import {toastr} from 'react-redux-toastr'
+import { asyncActionStart, asyncActionFinish, asyncActionError  } from '../../features/async/asyncActions';
 
 export const updateProfile = (user) => 
 async (dispatch, getState, {getFirebase}) => {
@@ -10,5 +11,49 @@ async (dispatch, getState, {getFirebase}) => {
     }
     catch(error){
         console.log(error)
+    }
+}
+
+export const uploadProfileImage = (file,  fileName) => 
+async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    const path = `${user.uid}/user_images`;
+    const options = {
+        name: fileName
+    };
+    try {
+
+    dispatch(asyncActionStart())
+        let uploadedFile = await firebase.uploadFile(path, file, null, options)
+
+        let downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+
+        let userDoc = await firestore.get(`users/${user.id}`)
+
+        if (!userDoc.data().photoURL) {
+            await firebase.updateProfile({
+                photoURL: downloadURL
+            });
+            await user.updateProfile({
+                    photoURL: downloadURL
+            })
+        }
+
+        await firestore.add({
+            collection: 'users',
+            doc: user.uid, 
+            subcollections: [{collection: 'photos'}]
+        }, {      
+            name: fileName,
+            url: downloadURL
+        })
+
+        dispatch(asyncActionFinish())
+
+    }catch(error) {
+        console.log(error)
+        dispatch(asyncActionError())
     }
 }
