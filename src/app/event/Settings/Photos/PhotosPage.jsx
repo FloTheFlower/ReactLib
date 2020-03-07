234c15cf67b2d70dfd9cf7f680535d99644f91e9
FloTheFlower <florentina.hofbauer@gmail.com>
2020-03-07 06:48:08 -0800

    photopage

--- src/app/event/Settings/Photos/PhotosPage.jsx
+++ src/app/event/Settings/Photos/PhotosPage.jsx
@@ -108,25 +108,7 @@ const handleCacnelCrop = () => {
 
 
         <Divider />
-        <Header sub color="teal" content="All Photos" />
-
-        <Card.Group itemsPerRow={5}>
-          <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-            <Button positive>Main Photo</Button>
-          </Card>
-
-          <Card>
-            <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Main
-              </Button>
-              <Button basic icon="trash" color="red" />
-            </div>
-          </Card>
-        </Card.Group> 
-
+       
       </Segment>
     );
   }
